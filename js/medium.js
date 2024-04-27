76d8ff957fd53469bdb72ada079c9587ad0ffdd9
stryderbateman <sbatem716@west-mec.org>
2024-04-26 18:15:41 -0700

    dont le me catch u slippn
    as a former hoodlum myself8epwo,uyhvtgcr

--- js/medium.js
+++ js/medium.js
@@ -7,17 +7,23 @@ $(document).ready(function() {
     var playerX = 20;
     var playerY = 20;
 
-        // Start the timer
-        function startTimer() {
-            timerInterval = setInterval(function() {
-                timeLeft--;
-                $('#timer').text('Time Left: ' + timeLeft + 's');
-                if (timeLeft <= 0) {
-                    clearInterval(timerInterval);
-                    alert("Time's up! You lose.");
-                }
-            }, 1000);
-        }
+    // Timer variables
+    var timeLeft = 120; // Set the initial time in seconds
+    var timerInterval;
+
+    // Start the timer
+    startTimer();
+
+    function startTimer() {
+        timerInterval = setInterval(function() {
+            timeLeft--;
+            $('#timeLeft').text(timeLeft);
+            if (timeLeft <= 0) {
+                clearInterval(timerInterval);
+                alert("Time's up! You lose.");
+            }
+        }, 1000);
+    }
 
     // Movement Functionality for player
     function movePlayer(dx, dy) {
@@ -32,7 +38,7 @@ $(document).ready(function() {
                 playerX = newX;
                 playerY = newY;
                 $('#player').css({ top: playerY + 'px', left: playerX + 'px' });
-                
+
                 var colors = ["#ff2d75", "#ff6b9f", "#c40051", "#4fc3dc", "#80e2f3", "#2a9cb6", "#8a22a9", "#b45ccf", "#5c0f82", "#9ad8ea", "#c6eaf3", "#6dbdd6", "#2b9bb2", "#5bc1d7", "#007d91"];
                 var randomIndex = Math.floor(Math.random() * colors.length);
                 var glowColor = colors[randomIndex]; // Get the color for the glowing effect
@@ -43,15 +49,14 @@ $(document).ready(function() {
                             'background-color': glowColor,
                             'box-shadow': shadow // Apply the glowing effect using box shadow
                         });
-                
-                
-}
+                }
 
             }
 
-            // Check to see if the player reaches the end
-            if (playerX === mazeWidth - 20 && playerY === mazeHeight - 20) {
-                alert("Congratulations you made it through the maze!");
+            // Check if the player reaches the end
+            if ($('#maze').find('.end-location').is('[style="top: ' + newY + 'px; left: ' + newX + 'px;"]')) {
+                clearInterval(timerInterval); // Stop the timer
+                alert("Congratulations! You've reached the end.");
             }
         }
     }
@@ -81,6 +86,8 @@ $(document).ready(function() {
             for (var x = 0; x < layout[y].length; x++) {
                 if (layout[y][x] === 1) {
                     wallCoordinates.push({ top: y * 20, left: x * 20 });
+                } else if (layout[y][x] === 2) {
+                    $('#maze').append('<div class="end-location" style="top: ' + (y * 20) + 'px; left: ' + (x * 20) + 'px;"></div>');
                 } else {
                     // Add grey square where there is an empty space
                     $('#maze').append('<div class="square" style="top: ' + (y * 20) + 'px; left: ' + (x * 20) + 'px;"></div>');
@@ -90,7 +97,6 @@ $(document).ready(function() {
         return wallCoordinates;
     }
 
-    // Maze layout as a grid of 1s and 0s
     var mazeLayout = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
@@ -130,9 +136,9 @@ $(document).ready(function() {
         [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
+    
     ];
 
     // Create Maze Walls from layout
@@ -143,4 +149,5 @@ $(document).ready(function() {
 
     // Create the player and append it to the board
     $('#maze').append('<div class="player" id="player" style="top: ' + playerY + 'px; left: ' + playerX + 'px;"></div>');
+
 });
